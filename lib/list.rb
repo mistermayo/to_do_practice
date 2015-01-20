f7f0350c15a2da25e1b303ac81edb67f960656ba
Tuck <tuckerjamesmayo@gmail.com>
2015-01-20 12:52:22 -0800

    Initial commit

---
+++ lib/list.rb
@@ -0,0 +1,39 @@
+class List
+  attr_reader(:name, :id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    returned_lists = DB.exec("SELECT * FROM lists;")
+    lists = []
+    returned_lists.each() do |list|
+      name = list.fetch("name")
+      id = list.fetch("id").to_i()
+      lists.push(List.new({:name => name, :id => id}))
+    end
+    lists
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+  
+
+  define_method(:==) do |another_list|
+    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  end
+
+  define_singleton_method(:find) do |id|
+    found_list = nil
+    List.all().each() do |list|
+      if list.id().==(id)
+        found_list = list
+      end
+    end
+    found_list
+  end
+end
